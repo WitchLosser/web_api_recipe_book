468d3900d031426bcf291eaa06afeab290b123af
Anton Kozak <anton.kozak230@gmail.com>
2023-03-07 23:25:55 +0200

    Implement DBcontext, clean architecture

---
+++ Core/Interfaces/IRecipeService.cs
@@ -0,0 +1,19 @@
+﻿using Core.Dtos;
+using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IRecipeService
+    {
+        Task<IEnumerable<RecipeDto>> GetAll();
+        Task<RecipeDto?> GetById(int id);
+        Task Create(RecipeDto recipe);
+        Task Edit(RecipeDto recipe);
+        Task Delete(int id);
+    }
+}
---
+++ Infrustructure/Configurations/RecipeConfigurations.cs
@@ -0,0 +1,29 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Configurations
+{
+    internal class RecipeConfigurations : IEntityTypeConfiguration<Recipe>
+    {
+        public void Configure(EntityTypeBuilder<Recipe> builder)
+        {
+            // Set Primary Key
+            builder.HasKey(x => x.Id);
+
+            // Set Property configurations
+            builder.Property(x => x.Name)
+                   .HasMaxLength(200)
+                   .IsRequired();
+
+            // set many to many relation
+            builder.HasMany(x => x.Ingredients).WithOne(x => x.Recipe).HasForeignKey(x => x.RecipeId);
+        }
+    }
+}
