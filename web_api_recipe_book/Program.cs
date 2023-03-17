7d55f463214c1fa6064bdb2e814f46ac08e77886
Anton Kozak <anton.kozak230@gmail.com>
2023-03-17 16:44:44 +0200

    Separate Swagger JWT Configuration to extensions

--- web_api_recipe_book/Program.cs
+++ web_api_recipe_book/Program.cs
@@ -18,35 +18,10 @@ string connStr = builder.Configuration.GetConnectionString("LocalDb");
 builder.Services.AddControllers()
     .AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
+//add jwt and SwaggerConfigurations
 builder.Services.AddJWT(builder.Configuration);
-builder.Services.AddSwaggerGen();
-builder.Services.AddSwaggerGen(c => {
-    c.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Title = "JWTToken_Auth_API",
-        Version = "v1"
-    });
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-    {
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
-    });
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
-        {
-            new OpenApiSecurityScheme {
-                Reference = new OpenApiReference {
-                    Type = ReferenceType.SecurityScheme,
-                        Id=" "
-                }
-            },
-            new string[] {}
-        }
-    });
-});
+builder.Services.AddSwaggerGenConfigurations();
+
 
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
